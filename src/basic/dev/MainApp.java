7080253c0f022256adf42d2ba42964448bf93005
le minh trung <you@example.com>
2021-10-27 14:53:59 +0700

    first commit

---
+++ src/basic/dev/MainApp.java
@@ -0,0 +1,28 @@
+package basic.dev;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 10, b= 5;
+	
+		boolean c = a > b;
+		c = a > b;
+		// phep cong
+		System.out.println(a+b);
+		System.out.println(a-b);
+		System.out.println(a*b);
+		System.out.println(b/a);
+		System.out.println(a > b ? "a lon hon b" : "b lon hon a");
+		System.out.println(a < b ? "a be hon b" : "b be hon a");
+		System.out.println(a >= b ? "a lon hon hoac bang b" : "b lon hon hoac bang a");
+		System.out.println(a <= b ? "a be  hon hoac bang b" : "b be hon  hoac bang a");
+	
+		
+	
+		
+		
+ 
+	}
+
+}
